781cbc6803e95f7596981771c73af5f9c6e61353
TitoGit17 <pacotito10008000@gmail.com>
2024-11-10 23:45:07 -0600

    Se agregó el proyecto

---
+++ administrator/cache/_media_version/4b0009871500096b269fb9c2a45a2d38-cache-_media_version-d6ed4b869516a48685affe3962f4e9c1.php
@@ -0,0 +1,1 @@
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"result";s:6:"32696f";s:6:"output";s:0:"";}
---
+++ administrator/cache/schemaorg/4b0009871500096b269fb9c2a45a2d38-cache-schemaorg-8c46d2c8ff1b6d654bbfd1926bb8b568.php
@@ -0,0 +1,1 @@
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"result";a:1:{i:0;a:6:{s:5:"@type";s:7:"Article";s:3:"@id";s:61:"http://192.168.1.2/tiendapesca/#/schema/com_content/article/1";s:4:"name";s:6:"INICIO";s:8:"headline";s:6:"INICIO";s:10:"inLanguage";s:5:"es-ES";s:8:"isPartOf";a:1:{s:3:"@id";s:52:"http://192.168.1.2/tiendapesca/#/schema/WebPage/base";}}}s:6:"output";s:0:"";}
